5f92457fe19d1af33578974c5aa9ef9217536617
Brandon Ayers <thedaemonofid@gmail.com>
2016-08-02 20:29:04 -0500

    Add files via upload
    Updated layout mostly.

--- Animators_Toolbox.py
+++ Animators_Toolbox.py
@@ -1,16 +1,11 @@
-##############################################
-##  thedaemon's Useful Animation Panel       #
-##  Thanks to Frankie Hobbins, Joel Daniels, #
-##  Julien Duroure, Hjalti Hjalmarsson,      #
-##  Bassam Kurdali, Luciano Munoz            #
-##############################################
+##  Thanks to Frankie Hobbins, Joel Daniels, Julien Duroure, Hjalti Hjalmarsson, Bassam Kurdali, Luciano Munoz, and anyone that I left out!
 
 bl_info = {
     "name": "Animator's Toolbox",
-    "author": "Brandon Ayers aka thedaemon",
-    "version": (0, 1, 5),
+    "author": "Brandon Ayers (thedaemon)",
+    "version": (0, 2, 0),
     "blender": (2, 77, 0),
-    "description": "Various animation tools.",
+    "description": "A set of tools specifically for animators.",
     "location": "View3D > Toolbar > Animation",
     "category": "Animation"
     }
@@ -20,12 +15,28 @@ from bpy.props import *
 
 KEYMAPS = list()
 
-#######################
-###### OPERATORS ######
-#######################
+# FEATURE: Jump forward/backward every N frames. Currently hardcoded variable.
+class AnimatorsToolboxFrameJump(bpy.types.Operator):
 
+    """Jump a number of frames forward/backwards"""
+    bl_idname = "screen.animatorstools_frame_jump"
+    bl_label = "Jump Frames"
+
+    forward = bpy.props.BoolProperty(default=True)
+
+    def execute(self, context):
+        scene = context.scene
+        framedelta = 4
+        if self.forward:
+            scene.frame_current = scene.frame_current + framedelta
+        else:
+            scene.frame_current = scene.frame_current - framedelta
+
+        return {"FINISHED"}
+
+# FEATURE: A toggle to keep the animator from selecting something other than the Armature.
 class ToggleSelectability(bpy.types.Operator):
-    """Toggles selectability of all objects leaving only Armature Selectable"""
+    """Turns off selection for all objects leaving only Armatures selectable"""
     bl_idname = "bone.toggleselectability"
     bl_label = "Armature Selection Only"
 
@@ -53,22 +64,7 @@ class ToggleSelectability(bpy.types.Operator):
                     ob.hide_select = not ob.hide_select
         return{'FINISHED'}
 
-class ToggleOpensubdiv(bpy.types.Operator):
-    """Toggles OpenSubdiv for all Objects for improved animation playback speed"""
-    bl_idname = "mesh.opensubdiv"
-    bl_label = "Mesh OpenSubdiv"
-    my_bool = bpy.props.BoolProperty(name="Toggle Option")
-
-    def execute(self,context):
-        for mm in (m for o in bpy.context.scene.objects for m in o.modifiers if m.type=='SUBSURF'):
-            if mm.use_opensubdiv == True:
-                mm.use_opensubdiv = False
-            else:
-                if mm.use_opensubdiv == False:
-                    mm.use_opensubdiv = True
-        return{'FINISHED'}
-
-## USELESS BECAUSE BLENDER ALREADY HAS THIS FREAKING COMMAND
+# Useless because blender already has this freaking command but I programmed it anyways. I may use it for specific axis template.
 class ClearAllTransforms(bpy.types.Operator):
     """Clears all transforms on the bone I hope"""
     bl_idname = "pose.clearall"
@@ -82,6 +78,52 @@ class ClearAllTransforms(bpy.types.Operator):
                 bpy.ops.pose.scale_clear()
         return{'FINISHED'}
 
+# FEATURE: A toggle for OpenSubdiv on all objects in scene with a Subdivision Surface Modifier.
+class ToggleOpensubdiv(bpy.types.Operator):
+    """Toggles OpenSubdiv for all Objects for improved animation playback speed"""
+    bl_idname = "mesh.opensubdiv"
+    bl_label = "Mesh OpenSubdiv"
+    # Does nothing, testing.
+    my_bool = bpy.props.BoolProperty(name="Toggle Option")
+
+    def execute(self,context):
+        for mm in (m for o in bpy.context.scene.objects for m in o.modifiers if m.type=='SUBSURF'):
+            if mm.use_opensubdiv == True:
+                mm.use_opensubdiv = False
+            else:
+                if mm.use_opensubdiv == False:
+                    mm.use_opensubdiv = True
+        return{'FINISHED'}
+
+# Feature: Turns OpenSubdiv on for all meshes with Subdivision Surface Modifiers for improved viewport performance.
+class OpensubdivOn(bpy.types.Operator):
+    bl_idname = "opensubdiv.on"
+    bl_label = "OpenSubdiv On"
+    
+    def execute(self,context):
+        for mm in (m for o in bpy.context.scene.objects for m in o.modifiers if m.type=='SUBSURF'):
+            if mm.use_opensubdiv == True:
+                mm.use_opensubdiv == True
+            else:
+                if mm.use_opensubdiv == False:
+                    mm.use_opensubdiv = True
+        return{'FINISHED'}
+
+# Feature: Turns OpenSubdiv on for all meshes with Subdivision Surface Modifiers for improved viewport performance.
+class OpensubdivOff(bpy.types.Operator):
+    bl_idname = "opensubdiv.off"
+    bl_label = "OpenSubdiv Off"
+    
+    def execute(self,context):
+        for mm in (m for o in bpy.context.scene.objects for m in o.modifiers if m.type=='SUBSURF'):
+            if mm.use_opensubdiv == True:
+                mm.use_opensubdiv = False
+            else:
+                if mm.use_opensubdiv == False:
+                    mm.use_opensubdiv = False
+        return{'FINISHED'}
+
+# FEATURE: Simple X-Ray toggle for Armature
 class ToggleXray(bpy.types.Operator):
     """Toggles X-Ray mode for bones"""
     bl_idname = "bone.togglexray"
@@ -92,103 +134,64 @@ class ToggleXray(bpy.types.Operator):
             if obj.type == 'ARMATURE':
                 obj.show_x_ray = not obj.show_x_ray
         return{'FINISHED'}
-## CURRENTLY UNUSED BUT I WILL USE AT A LATER DATE
-class AddFakeUsers(bpy.types.Operator):
-    """Add Fake users to all actions to stop them getting deleted on save"""
-    bl_idname = "bone.addfakeusers"
-    bl_label = "Unused"
 
-    def execute(self, context):
-        for a in bpy.data.actions:
-            a.use_fake_user=True
-        return{'FINISHED'}
-## CURENTELY UNUSED BUT I WILL USE AT A LATER DATE
-class RemoveFakeUsers(bpy.types.Operator):
-    """Remove Fake users from all actions to help them getting removed on save"""
-    bl_idname = "bone.removefakeusers"
-    bl_label = "Unused"
-
-    def execute(self, context):
-        for a in bpy.data.actions:
-            a.user_clear()
-            a.use_fake_user=False
-        return{'FINISHED'}
-
-# FEATURE: Jump forward/backward every N frames
-class AnimatorsToolboxFrameJump(bpy.types.Operator):
-
-    """Jump a number of frames forward/backwards"""
-    bl_idname = "screen.animatorstools_frame_jump"
-    bl_label = "Jump Frames"
-
-    forward = bpy.props.BoolProperty(default=True)
-
-    def execute(self, context):
-        scene = context.scene
-        framedelta = 4
-        if self.forward:
-            scene.frame_current = scene.frame_current + framedelta
-        else:
-            scene.frame_current = scene.frame_current - framedelta
-
-        return {"FINISHED"}
-
-################
-###### UI ######
-################
+## UI ##
 
 class AnimatorsToolBox(bpy.types.Panel):
-    """Creates a custom rigging Panel in the 3D View"""
-    # The header of the panel
+    """Creates a custom Animator Panel in the 3D View"""
     bl_label = "Animator's Toolbox"
     bl_idname = "ANIM_TOOLBOX"
-    # This is where the panel is located. Currently in the 3d viewport, view_3d
     bl_space_type = 'VIEW_3D'
-    # This places it in the left Tools Menu.
     bl_region_type = 'TOOLS'
-    # This is the Tab that the panel is located.
     bl_category = "Animation"
     # This limits the panel to the listed context. IE it won't show up unless you are in this mode!
     #bl_context = "posemode"
 
     def draw(self, context):
-        layout = self.layout
         # Defining shortcuts for commands
+        layout = self.layout
         obj = context.object
-        #userpref = context.user_preferences
-        #ob = context.object
-        #edit = userpref.edit
-        #oscon = context.scene.objects
-        #toolsettings = context.tool_settings
-        #scene = context.scene
-        #screen = context.screen
+        userpref = context.user_preferences
+        obj = context.object
+        edit = userpref.edit
+        toolsettings = context.tool_settings
+        scene = context.scene
+        screen = context.screen
 
-
-#############################
+#-------Keying
         col = layout.column(align=True)
-        col.label(text="Keying")
+        col.label(text="Keyframes:")
         row = layout.row(align=True)
-        row.prop(context.tool_settings, "use_keyframe_insert_auto", text="", toggle=True)
-        if context.tool_settings.use_keyframe_insert_auto:
-            row.prop(context.tool_settings, "use_keyframe_insert_keyingset", text="", toggle=True)
-            if context.screen.is_animation_playing:
+        row.prop(toolsettings, "use_keyframe_insert_auto", text="", toggle=True)
+        if toolsettings.use_keyframe_insert_auto:
+            row.prop(toolsettings, "use_keyframe_insert_keyingset", text="", toggle=True)
+            if screen.is_animation_playing:
                 subsub = row.row()
-                subsub.prop(context.tool_settings, "use_record_with_nla", toggle=True)
-        row.prop_search(context.scene.keying_sets_all, "active", context.scene, "keying_sets_all", text="")
+                subsub.prop(toolsettings, "use_record_with_nla", toggle=True)
+        row.prop_search(scene.keying_sets_all, "active", scene, "keying_sets_all", text="")
         row.operator("screen.animation_play", text="", icon='PLAY')
         row.operator("screen.keyframe_jump", text="", icon='PREV_KEYFRAME').next = False
-        row.operator("screen.keyframe_jump", text="", icon='NEXT_KEYFRAME').next = True
-###########################
+        row.operator("screen.keyframe_jump", text="", icon='NEXT_KEYFRAME').next = True    
+#-------Pose Tools
         col = layout.column(align=True)
-        col.label(text="In Betweens")
-        row = col.row()
+        col.label(text="Pose:")
+        row = col.row(align=True)
+        row.operator("pose.copy", text="Copy")
+        row.operator("pose.paste", text="Paste")
+        row.operator("pose.paste", text="Flipped").flipped = True
+        col = layout.column(align=True)
+        row = col.row(align=True)
         row.operator("pose.breakdown", text="Breakdowner")
         row.operator("pose.push", text="Push")
         row.operator("pose.relax", text="Relax")
-############################
         col = layout.column(align=True)
-        col.label(text="Reset Transforms")
-        ## This makes the buttons grouped
+        row = col.row()
+        row.prop(context.active_object.data, "use_auto_ik", text="Auto IK")
+        row.prop(obj, "show_x_ray", text="X Ray")       
+        
+#-------Reset Transforms
+        col = layout.column(align=True)
+        col.label(text="Reset Transforms:")
         row = layout.row(align=True)
         row.operator("pose.loc_clear", text="Location")
         row.operator("pose.rot_clear", text="Rotation")
@@ -196,43 +199,37 @@ class AnimatorsToolBox(bpy.types.Panel):
         col = layout.column(align=True)
         row = col.row()
         row.operator("pose.transforms_clear", text="Reset All")
-
-############################
-        col.label(text="New Key Type")
+        
+#-------Optimizations
         col = layout.column(align=True)
-        col.prop(context.user_preferences.edit, "keyframe_new_interpolation_type", text='Keys')
-        col.prop(context.user_preferences.edit, "keyframe_new_handle_type", text="Handles")
-##############################
-        col = layout.column(align=True)
-        col.label(text="Optimization")
-        #row = layout.row(align=True)
-        ## This makes each button seperate
-        row = col.row()
-        row.operator("mesh.opensubdiv", text ="OpenSubdiv")
-        row.prop(context.object, "show_x_ray", text="X-Ray")
-        #row.prop("mesh.opensubdiv", text = "OpenSubdiv")
-        col = layout.column(align=True)
-        #row = col.row()
-        #row = layout.row(align=True)
-        #row.operator("bone.togglexray", text="X-Ray")
-
+        col.label(text="Optimizations:")
+        #col.layout.column(align=True)
+        #col.label(text="OpenSubdiv")
+        row = layout.row(align=True)
+        row.label(text="OpenSubdiv")
+        row.operator("opensubdiv.on", text="On")
+        row.operator("opensubdiv.off", text="Off")
         row = layout.row(align=True)
         row.operator("bone.toggleselectability", text="Select Armature Only")
-######################
-###### REGISTER ######
-######################
+
+#-------New Key Type
+        col = layout.column(align=True)
+        col.label(text="New Key Type:")
+        col.prop(edit, "keyframe_new_interpolation_type", text='Keys')
+        col.prop(edit, "keyframe_new_handle_type", text="Handles")
+
 
 def register():
 
-    bpy.utils.register_class(AddFakeUsers)
-    bpy.utils.register_class(RemoveFakeUsers)
+    bpy.utils.register_class(AnimatorsToolboxFrameJump)
     bpy.utils.register_class(ToggleSelectability)
-    bpy.utils.register_class(ToggleOpensubdiv)
-    bpy.utils.register_class(ToggleXray)
     bpy.utils.register_class(ClearAllTransforms)
     bpy.utils.register_class(AnimatorsToolBox)
-    bpy.utils.register_class(AnimatorsToolboxFrameJump)
-    # register keyboard shortcuts for Frame Jump
+    bpy.utils.register_class(ToggleOpensubdiv)
+    bpy.utils.register_class(OpensubdivOff)
+    bpy.utils.register_class(OpensubdivOn)
+    bpy.utils.register_class(ToggleXray)
+
     wm = bpy.context.window_manager
     kc = wm.keyconfigs.addon
     km = kc.keymaps.new(name="Frames")
@@ -247,17 +244,19 @@ def register():
 
 def unregister():
 
-    bpy.utils.unregister_class(AddFakeUsers)
-    bpy.utils.unregister_class(RemoveFakeUsers)
+    bpy.utils.unregister_class(AnimatorsToolboxFrameJump)
     bpy.utils.unregister_class(ToggleSelectability)
-    bpy.utils.unregister_class(ToggleOpensubdiv)
-    bpy.utils.unregister_class(ToggleXray)
     bpy.utils.unregister_class(ClearAllTransforms)
     bpy.utils.unregister_class(AnimatorsToolBox)
-    bpy.utils.unregister_class(AnimatorsToolboxFrameJump)
+    bpy.utils.unregister_class(ToggleOpensubdiv)
+    bpy.utils.unregister_class(OpensubdivOff)
+    bpy.utils.unregister_class(OpensubdivOn)
+    bpy.utils.unregister_class(ToggleXray)
+    
     for km, kmi in KEYMAPS:
         km.keymap_items.remove(kmi)
     KEYMAPS.clear()
 
+# The End
 if __name__ == "__main__":
     register()
